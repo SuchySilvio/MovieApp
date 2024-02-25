ceff0f1cbf6a4deaacd21dcb2ec57ccb953b8db5
Silvio Suchy <suchysilvio9@gmail.com>
2024-02-24 23:55:54 -0800

    testing2

--- vite-project/server.js
+++ vite-project/server.js
@@ -1,10 +1,8 @@
 import express from 'express';
-import mongoose from 'mongoose';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 import cors from 'cors';
-import { Build } from './models/Build.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -14,6 +12,8 @@ const port = 3001;
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, './dist')));
+app.use(cors());
+
 
 // Static files and catch-all route should be defined after API routes
 
